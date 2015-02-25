9546fc32cde0f8e3f9913dd94f0cc266b109f2af
bordun <bordun@z97x>
2015-02-25 11:09:22 +0100

    doc & locals

--- presskit/local_en.php
+++ presskit/local_en.php
@@ -2,6 +2,8 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+    //C
+    'credits' => '<small><em>Copyleft <a href="http://ammd.net">AMMD</a> 2015 (GPL3) | background image by <a href="https://unsplash.com/taylorleopold">Taylor Leopold</a> | Source code & doc @ <a href="https://github.com/jean-emmanuel/presskit">https://github.com/jean-emmanuel/presskit</a></small>',
 
     // T
     'telecharger' => 'Download the press kit',
